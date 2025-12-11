608696a380193a1c6e1ca854b6056fdf1b03163d
DEMBO DIAOUNE <diaouned@yahoo.fr>
2025-12-11 11:24:14 +0100

    premier commit

---
+++ Hackathon2/Hackathon2/Hackathon2App.swift
@@ -0,0 +1,17 @@
+//
+//  Hackathon2App.swift
+//  Hackathon2
+//
+//  Created by Dembo on 11/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Hackathon2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
